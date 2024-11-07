d33f8ecea5a9785223ba36a9cc6ed0ca875449df
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2024-11-07 13:00:20 +0530

    Authentication using jwt token

---
+++ src/middlewares/jwt.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+const jwtAuth = (req, res, next) => {
+  // 1. Create jwt token
+  const token = req.headers["authorization"];
+  // 2. if no token then return error
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+  // 3.Check if token is valid
+  try {
+    const payload = jwt.verify(token, "9MbltR1RpKRLuiqI92awST3TVoqNBzBB");
+    console.log(payload);
+    //4. return error
+  } catch (error) {
+    return res.status(401).send("Unauthorized");
+  }
+  next();
+};
+export default jwtAuth;
