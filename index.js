4fc66d4a42c8e5d66010f899aabbc6f780496cc3
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2024-11-08 18:31:03 +0530

    Implementing logger middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import userRouter from "./src/features/user/user.routes.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
 import wishlistRouter from "./src/features/wishlist/wishlistItems.routes.js";
 import apiDocs from "./swagger.json" with { type: "json" };
+import loggerMiddleware from "./src/middlewares/logger.middleware.js";
 const server = express();
 const PORT = 8000;
 // CORSE policy configuration using CORS library
@@ -16,8 +17,9 @@ server.use(cors(corsOptions))
 server.use(express.json());
 // For all requrests related to movie, redirect to movie routes
 server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
+server.use(loggerMiddleware)
 server.use("/api/movies", jwtAuth, movieRouter);
-server.use("/api/wishlistItems", jwtAuth, wishlistRouter);
+server.use("/api/wishlistItems", loggerMiddleware, jwtAuth, wishlistRouter);
 server.use("/api/users", userRouter);
 server.get("/", (req, res) => {
   res.send("Welcome to RDSCinemas !!!");
