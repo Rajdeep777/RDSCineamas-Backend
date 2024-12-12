b8ace7b062bd8d1e8b0792d086f2fd26bb1ec3ca
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2024-12-12 14:44:18 +0530

    Updating add movie route

--- src/features/movie/movie.routes.js
+++ src/features/movie/movie.routes.js
@@ -15,7 +15,7 @@ movieRouter.get("/ratings", (req, res) => {
 movieRouter.get("/categories", (req, res) => {
   movieController.getAllCategories(req, res);
 });
-movieRouter.post("/", upload.single("imageUrl"), (req, res, next) => {
+movieRouter.post("/add", upload.single("imageUrl"), (req, res, next) => {
   movieController.addMovie(req, res, next);
 });
 movieRouter.post("/rate", (req, res, next) => {
